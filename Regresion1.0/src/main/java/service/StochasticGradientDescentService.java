24b7fbbdf4afdc75aa465dd734df120952d66081
abulmez <bulmezalexandru97@gmail.com>
2018-04-30 00:05:23 +0300

    Working regression with evolutionary algorithm + least squares + gradient descent

---
+++ Regresion1.0/src/main/java/service/StochasticGradientDescentService.java
@@ -0,0 +1,62 @@
+package service;
+
+import repository.FileRepo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class StochasticGradientDescentService {
+
+    FileRepo repo;
+
+    public StochasticGradientDescentService(FileRepo repo) {
+        this.repo = repo;
+    }
+
+    public Double getError(ArrayList<Double> factor,Integer currentUsedRow,Integer resultColumn){
+
+        Double currentValue = factor.get(0);
+        for(int j=1;j<factor.size();j++){
+            currentValue+=repo.getDataValue(currentUsedRow,j-1)*factor.get(j);
+        }
+        return repo.getResultValue(currentUsedRow,resultColumn)-currentValue;
+    }
+
+    public ArrayList<Double> solve(Integer numberOfIterations,Double learningRate,Integer resultColumn){
+        learningRate/=repo.getDataMatrixNumberOfRows();
+        ArrayList<Integer> indexes = new ArrayList<>();
+        Integer counter = 0;
+        ArrayList<Double> factor = new ArrayList<>();
+        for(int i=0;i<repo.getDataMatrixNumberOfRows();i++){
+            indexes.add(i);
+        }
+        Collections.shuffle(indexes);
+        Random r = new Random();
+        for(int i=0;i<repo.getDataMatrixNumberOfColumns()+1;i++){
+            factor.add(Math.random());
+        }
+
+        for(int i=0;i<numberOfIterations;i++) {
+            Integer row = indexes.get(counter);
+            Double error = getError(factor, row, resultColumn);
+            factor.set(0, (factor.get(0) - learningRate * error));
+            for (int j = 1; j < factor.size(); j++) {
+                factor.set(j, (factor.get(j) - learningRate * error * repo.getDataValue(row, j - 1)));
+            }
+            counter += 1;
+            if(counter.equals(repo.getDataMatrixNumberOfRows())){
+                counter = 0;
+                Collections.shuffle(indexes);
+                learningRate/=10;
+            }
+            /*
+            if(counter.equals(1000)){
+                counter = 0;
+                learningRate/=10;
+            }
+            */
+        }
+        return factor;
+    }
+}
--- Regresion1.0/src/main/java/ui/UI.java
+++ Regresion1.0/src/main/java/ui/UI.java
@@ -4,15 +4,17 @@ import model.Matrix;
 import repository.FileRepo;
 import service.EvolutionaryAlgorithmService;
 import service.LeastSquaresService;
+import service.StochasticGradientDescentService;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UI {
 
-    EvolutionaryAlgorithmService evolutionaryAlgorithmService;
-    LeastSquaresService leastSquaresService;
-    Scanner sc;
+    private EvolutionaryAlgorithmService evolutionaryAlgorithmService;
+    private LeastSquaresService leastSquaresService;
+    private StochasticGradientDescentService stochasticGradientDescentService;
+    private Scanner sc;
     public UI(){
         sc = new Scanner(System.in);
     }
@@ -26,50 +28,113 @@ public class UI {
         FileRepo repo = new FileRepo("parkinsons_updrs_training.txt", "parkinsons_updrs_testing.txt");
         evolutionaryAlgorithmService = new EvolutionaryAlgorithmService(repo);
         leastSquaresService = new LeastSquaresService(repo);
-
-        if(x==1) {
-
+        stochasticGradientDescentService = new StochasticGradientDescentService(repo);
 
 
-            // System.out.print("done");
-            System.out.println("Dati numarul de generatii:");
-            int generationNumber = sc.nextInt();
-            System.out.println("Dati marimea populatiei:");
-            int populationSize = sc.nextInt();
-            System.out.println("Dati factorul de mutatie: (valoare intre 0 si 10)");
-            int mutatianRate = sc.nextInt();
-            System.out.println("Dati rezultatul pentru care se va efectua regresia:");
-            System.out.println("1.motor_UPDRS");
-            System.out.println("2.total_UPDRS");
-            int resultColumn = sc.nextInt();
-            ArrayList<Double> result = evolutionaryAlgorithmService.solve(generationNumber, populationSize, mutatianRate, resultColumn - 1);
-            System.out.println("Coeficientii determinati:");
-            for (Double rez : result) {
-                System.out.print(rez + " ");
+        Boolean merge = true;
+        while(merge) {
+
+            System.out.println("Alegeti algoritmul de antrenare:");
+            System.out.println("1.Cele mai mici patrate");
+            System.out.println("2.Gradient descendent");
+            System.out.println("3.Algoritm evolutiv");
+            System.out.println("4.Iesire");
+            x = sc.nextInt();
+
+
+            switch (x) {
+
+
+                case 3: {
+                    // System.out.print("done");
+                    System.out.println("Dati numarul de generatii:");
+                    int generationNumber = sc.nextInt();
+                    System.out.println("Dati marimea populatiei:");
+                    int populationSize = sc.nextInt();
+                    System.out.println("Dati factorul de mutatie: (valoare intre 0 si 10)");
+                    int mutatianRate = sc.nextInt();
+                    System.out.println("Dati rezultatul pentru care se va efectua regresia:");
+                    System.out.println("1.motor_UPDRS");
+                    System.out.println("2.total_UPDRS");
+                    int resultColumn = sc.nextInt();
+                    ArrayList<Double> result = evolutionaryAlgorithmService.solve(generationNumber, populationSize, mutatianRate, resultColumn - 1);
+                    System.out.println("Coeficientii determinati:");
+                    for (Double rez : result) {
+                        System.out.print(rez + " ");
+                    }
+                    System.out.println();
+                    System.out.println();
+                    System.out.println("Valoarea reala | Valoarea Determinata | Eroare");
+                    System.out.println();
+                    Matrix<Double> evaluationResult = evolutionaryAlgorithmService.test(result, resultColumn - 1);
+                    Double error = 0.0;
+                    for (int i = 0; i < evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows(); i++) {
+                        System.out.println(evaluationResult.get(i, 0) + " " + evaluationResult.get(i, 1) + " " + Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1)));
+                        error += Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1));
+                    }
+                    System.out.println();
+                    System.out.println("Eroarea medie: "+error/evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows());
+                    break;
+                }
+
+                case 1: {
+                    System.out.println("Dati rezultatul pentru care se va efectua regresia:");
+                    System.out.println("1.motor_UPDRS");
+                    System.out.println("2.total_UPDRS");
+                    int resultColumn = sc.nextInt();
+                    ArrayList<Double> result = leastSquaresService.solve(resultColumn - 1);
+                    System.out.println("Coeficientii determinati:");
+                    for (Double rez : result) {
+                        System.out.print(rez + " ");
+                    }
+                    System.out.println();
+                    System.out.println("Valoarea reala | Valoarea Determinata | Eroare");
+                    System.out.println();
+                    Matrix<Double> evaluationResult = evolutionaryAlgorithmService.test(result, resultColumn - 1);
+                    Double error = 0.0;
+                    for (int i = 0; i < evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows(); i++) {
+                        System.out.println(evaluationResult.get(i, 0) + " " + evaluationResult.get(i, 1) + " " + Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1)));
+                        error += Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1));
+                    }
+                    System.out.println();
+                    System.out.println("Eroarea medie: "+error/evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows());
+                    break;
+                }
+                case 2: {
+                    System.out.println("Dati rezultatul pentru care se va efectua regresia:");
+                    System.out.println("1.motor_UPDRS");
+                    System.out.println("2.total_UPDRS");
+                    int resultColumn = sc.nextInt();
+                    System.out.println("Dati gradul de invatare:");
+                    Double learningRate = sc.nextDouble();
+                    System.out.println("Dati numarul de iteratii:");
+                    int numberOfIterations = sc.nextInt();
+                    ArrayList<Double> result = stochasticGradientDescentService.solve(numberOfIterations, learningRate, resultColumn-1);
+                    System.out.println("Coeficientii determinati:");
+                    for (Double rez : result) {
+                        System.out.print(rez + " ");
+                    }
+                    System.out.println();
+                    System.out.println("Valoarea reala | Valoarea Determinata | Eroare");
+                    System.out.println();
+                    Matrix<Double> evaluationResult = evolutionaryAlgorithmService.test(result, resultColumn - 1);
+                    Double error = 0.0;
+                    for (int i = 0; i < evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows(); i++) {
+                        System.out.println(evaluationResult.get(i, 0) + " " + evaluationResult.get(i, 1) + " " + Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1)));
+                        error += Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1));
+                    }
+                    System.out.println();
+                    System.out.println("Eroarea medie: "+error/evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows());
+                    break;
+                }
+                case 4: {
+                    merge = false;
+                    break;
+                }
+
+                default:
+                    System.out.println("Optiune invalida! Mai incercati!");
             }
-            System.out.println();
-            System.out.println();
-            System.out.println("Valoarea reala | Valoarea Determinata | Eroare");
-            System.out.println();
-            Matrix<Double> evaluationResult = evolutionaryAlgorithmService.test(result, resultColumn - 1);
-            for (int i = 0; i < evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows(); i++) {
-                System.out.println(evaluationResult.get(i, 0) + " " + evaluationResult.get(i, 1) + " " + Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1)));
-            }
-        }
-        else {
-            System.out.println("Dati rezultatul pentru care se va efectua regresia:");
-            System.out.println("1.motor_UPDRS");
-            System.out.println("2.total_UPDRS");
-            int resultColumn = sc.nextInt();
-            ArrayList<Double> result = leastSquaresService.solve(resultColumn-1);
-            System.out.println();
-            System.out.println("Valoarea reala | Valoarea Determinata | Eroare");
-            System.out.println();
-            Matrix<Double> evaluationResult = evolutionaryAlgorithmService.test(result, resultColumn - 1);
-            for (int i = 0; i < evolutionaryAlgorithmService.repo.getTestResultMatrixNumberOfRows(); i++) {
-                System.out.println(evaluationResult.get(i, 0) + " " + evaluationResult.get(i, 1) + " " + Math.abs(evaluationResult.get(i, 0) - evaluationResult.get(i, 1)));
-            }
-
         }
 
 
